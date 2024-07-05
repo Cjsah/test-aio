38277bb63ba028605a8fea969a56bd595d3f5505
Cjsah <cjsah@cjsah.net>
2024-07-05 15:56:44 +0800

    tip

--- java/src/main/java/net/cjsah/main/template/Html2Word.java
+++ java/src/main/java/net/cjsah/main/template/Html2Word.java
@@ -6,6 +6,7 @@ import com.deepoove.poi.XWPFTemplate;
 import com.deepoove.poi.config.Configure;
 import lombok.extern.slf4j.Slf4j;
 import net.cjsah.data.UpdateReading;
+import net.cjsah.util.HtmlUtil;
 import net.cjsah.util.JsonUtil;
 import org.ddr.poi.html.HtmlRenderPolicy;
 
@@ -21,13 +22,17 @@ public class Html2Word {
     public static void main(String[] args) throws IOException {
         File input = new File("./study-template.docx");
         File output = new File("./result.docx");
+        File templateFile = new File("./template.json");
+        File articleFile = new File("./article.json");
 
-        String s = FileUtil.readUtf8String(new File("article.json"));
+        String s = FileUtil.readUtf8String(templateFile);
+        JSONObject template = JsonUtil.str2Obj(s, JSONObject.class);
+
+        s = FileUtil.readUtf8String(articleFile);
         JSONObject json = JsonUtil.str2Obj(s, JSONObject.class);
         UpdateReading article = UpdateReading.fromJson(json);
 
         //        String content = HtmlUtil.ofContent(title);
-
 
         HtmlRenderPolicy htmlRenderPolicy = new HtmlRenderPolicy();
         Configure config = Configure.builder()
@@ -46,9 +51,7 @@ public class Html2Word {
         data.put("vocabulary", 20);
         data.put("count", 30);
 
-        data.put("tip", "<p style=\"text-indent: 24px;\">请逐字逐句翻译，确保看懂每一句话，看懂文章再做题，不要为了做题而读文章，不要跳略读，不要先看题再看文章。</p>" +
-                "<p style=\"text-indent: 24px;\">我们的目标是在高于80%正确率的前提下，阅读文章每分钟在80个单词以上。</p>    " +
-                "<p style=\"text-indent: 24px;\">经过刻意学习后，你一定能实现这个目标。当目标达成时，英语学习对于你来说轻松无比，拿高分顺理成章。</p>");
+        data.put("tip", HtmlUtil.ofIndent(template.getList("tip", String.class)));
 
 
 
@@ -63,14 +66,14 @@ public class Html2Word {
 //        data.put("", null);
 //        data.put("article", content);
 
-        try (
-                XWPFTemplate template = XWPFTemplate.compile(input, config).render(data);
-                BufferedOutputStream outputStream = FileUtil.getOutputStream(output)
-        ) {
-            template.write(outputStream);
-        } catch (IOException e) {
-            log.error("err", e);
-        }
+//        try (
+//                XWPFTemplate template = XWPFTemplate.compile(input, config).render(data);
+//                BufferedOutputStream outputStream = FileUtil.getOutputStream(output)
+//        ) {
+//            template.write(outputStream);
+//        } catch (IOException e) {
+//            log.error("err", e);
+//        }
 
     }
 
--- java/src/main/java/net/cjsah/util/HtmlUtil.java
+++ java/src/main/java/net/cjsah/util/HtmlUtil.java
@@ -1,10 +1,24 @@
 package net.cjsah.util;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class HtmlUtil {
     public static String ofContent(String content) {
         return "<html><body>" + content + "</body></html>";
     }
 
-    public static String ofWord(String content, String tag) {}
+    public static String ofIndent(List<String> tips) {
+        String content = tips.stream()
+                .map(it -> "<p style=\"text-indent: 24px;\">" + it + "</p>")
+                .collect(Collectors.joining());
+        return ofContent(content);
+    }
+
+//    public static String ofWord(String content, String tag) {
+//
+//
+//
+//    }
 
 }
